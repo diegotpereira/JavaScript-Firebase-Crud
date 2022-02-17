829727fe39ee7e2235d6bb0da642edb2f3f29544
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-02-17 16:01:53 -0300

    criação do metodo listar jogador.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+// Inciar Lista
+$(document).ready(function() {
+    listaJogadores()
+})
+
+// Adicionar Jogador
 function adicionarOuAtualizar(event) {
     let id = document.getElementById('id')
 
@@ -13,7 +19,7 @@ function addJogador(event) {
     let addRef = firebase.database().ref('jogador')
     jogador.id = addRef.push().key
     addRef.child(jogador.id).set(jogador)
-
+    listaJogadores()
     event.preventDefault();
 }
 
@@ -25,4 +31,37 @@ function getDadosEntradaJogador() {
     idade.value = ''
 
     return dado
+}
+
+// Lista de todos os Jogadores
+function listaJogadores() {
+    $('.lista').html('')
+
+    let jogadorRef = firebase.database().ref('jogador')
+    jogadorRef.on('value', function(snap) {
+        let jogador = snap.val()
+
+        for (var dado in jogador) {
+            if (!jogador.hasOwnProperty(dado)) continue
+
+            let obj = jogador[dado]
+
+            criarCartaoLista(obj)
+        }
+    })
+}
+
+function criarCartaoLista(dado) {
+    $('.lista').append(
+        `<div class="card mt-2">
+		   <div class="card-body">
+		     <div class="card-title">Nome: ${dado.nome} <br>Idade: ${dado.idade}</div>
+			 <a href="#" class="btn btn-danger" onclick="deletarJogador('${dado.id}')">
+			   <i class="fas fa-trash"></i>
+			 </a>
+			 <a href="#" class="btn btn-warning" onclick="editarJogador('${dado.id}')">
+			   <i class="fas fa-pencil-alt"></i>
+			 </a>
+		   </div>
+		</div>`)
 }
